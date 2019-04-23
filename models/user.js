9e72f77777cd4562ce25e10fb4b5a900b8ba97ca
Jenkins <lyonjenkins@gmail.com>
2019-04-23 07:35:00 -0500

    Added steamProfile field for userSchema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
     registrationDate: Date,
+    steamProfile: String,
     unit: {type: Object, default: {company: "none", platoon: "none", squad: "none"}},
     status: {type: String, default: "none"},
     rank: {type: String, default: "none"},
