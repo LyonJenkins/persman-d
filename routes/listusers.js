139e9d263c7cd2ecdc12f10f499b0d3fcb4766dc
Jenkins <lyonjenkins@gmail.com>
2019-06-01 14:11:08 -0500

    Only displays squads that are populated

--- routes/listusers.js
+++ routes/listusers.js
@@ -2,11 +2,23 @@ const express = require("express"), router = express.Router(), passport = requir
 const admin = 5, recruiter = 4, officer = 3, nco = 2, enlisted = 1, guest = 0;
 
 router.get("/listusers", isLoggedIn, function(req, res){
+    let platoons = [];
+    const sortingSquads = ["Platoon HQ", "First Squad", "Second Squad", "Third Squad", "Fourth Squad"];
+    let squads = [];
     User.find({}, function(err, allUsers){
        if(err) {
           console.log(err);
        } else {
-          res.render("listusers", {users: allUsers});
+           allUsers.forEach(function(user){
+              const platoon = user.unit.platoon;
+              const squad = user.unit.squad;
+              if(platoons.indexOf(platoon) === -1 && platoon !== "none") platoons.push(platoon);
+              if(squads.indexOf(squad) === -1 && squad !== "none") squads.push(squad);
+              squads.sort(function(a, b){
+                 return sortingSquads.indexOf(a) - sortingSquads.indexOf(b);
+              });
+           });
+          res.render("listusers", {users: allUsers, platoons: platoons, squads: squads});
        }
     })
  });
