dd831374802763b6fa1352b51fb85f1a1546c7d2
Jenkins <lyonjenkins@gmail.com>
2019-06-30 00:45:44 -0500

    Adding a password reset system

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require("express"), app = express(), bodyParser = require("body-parser"),
-    mongoose = require("mongoose"), passport = require("passport"), LocalStrategy = require("passport-local"),
-    User = require("./models/user"), authRoutes = require("./routes/index"),
-    listUsersRoutes = require("./routes/listusers"), opCenterRoutes = require("./routes/opcenter"),
-    userRoutes = require("./routes/user.js"), calendarRoutes = require("./routes/calendar.js");
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    authRoutes = require("./routes/index"),
+    listUsersRoutes = require("./routes/listusers"),
+    opCenterRoutes = require("./routes/opcenter"),
+    userRoutes = require("./routes/user.js"),
+    calendarRoutes = require("./routes/calendar.js"),
+    flash = require("connect-flash"),
+    session = require('express-session'),
+    cookieParser = require("cookie-parser");
 
 mongoose.connect("mongodb://localhost:27017/persman", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.set('useFindAndModify', false);
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use(cookieParser('secret'));
 
 //PASSPORT
 app.use(require("express-session")({
@@ -17,6 +28,7 @@ app.use(require("express-session")({
    saveUninitialized: false
 }));
 
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -25,6 +37,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
-const express = require("express"), router = express.Router(), passport = require("passport"), Calendar = require("../models/calendar"), User = require("../models/user"), flash = require("connect-flash");
+const express = require("express"),
+    router = express.Router(),
+    passport = require("passport"),
+    Calendar = require("../models/calendar"),
+    User = require("../models/user"),
+    async = require("async"),
+    nodemailer = require("nodemailer"),
+    crypto = require("crypto");
 
 router.get("/", function(req, res){
     Calendar.find({}, function(err, allEvents){
@@ -15,6 +22,121 @@ router.get("/", function(req, res){
     });
  });
 
+router.get("/forgot", function(req,res){
+    res.render("forgot");
+});
+
+router.post('/forgot', function(req, res, next) {
+    async.waterfall([
+        function(done) {
+            crypto.randomBytes(20, function(err, buf) {
+                var token = buf.toString('hex');
+                done(err, token);
+            });
+        },
+        function(token, done) {
+            User.findOne({ email: req.body.email }, function(err, user) {
+                if (!user) {
+                    req.flash('error', 'No account with that email address exists.');
+                    return res.redirect('/forgot');
+                }
+
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+                user.save(function(err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        function(token, user, done) {
+            var smtpTransport = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'persmanwebsite@gmail.com',
+                    pass: 'PERSMANWEBSITEMESTIA^&%$#'
+                }
+            });
+            var mailOptions = {
+                to: user.email,
+                from: 'persmanwebsite@gmail.com',
+                subject: 'PERSMAN Password Reset',
+                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                    'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                    'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                done(err, 'done');
+            });
+        }
+    ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+    });
+});
+
+router.get('/reset/:token', function(req, res) {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+            console.log("password token invalid");
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot');
+        }
+        res.render('reset', {token: req.params.token});
+    });
+});
+
+router.post('/reset/:token', function(req, res) {
+    async.waterfall([
+        function(done) {
+            User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+                if (!user) {
+                    req.flash('error', 'Password reset token is invalid or has expired.');
+                    return res.redirect('back');
+                }
+                if(req.body.password === req.body.confirm) {
+                    user.setPassword(req.body.password, function(err) {
+                        user.resetPasswordToken = undefined;
+                        user.resetPasswordExpires = undefined;
+
+                        user.save(function(err) {
+                            req.logIn(user, function(err) {
+                                done(err, user);
+                            });
+                        });
+                    })
+                } else {
+                    req.flash("error", "Passwords do not match.");
+                    return res.redirect('back');
+                }
+            });
+        },
+        function(user, done) {
+            var smtpTransport = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'learntocodeinfo@gmail.com',
+                    pass: process.env.GMAILPW
+                }
+            });
+            var mailOptions = {
+                to: user.email,
+                from: 'learntocodeinfo@mail.com',
+                subject: 'Your password has been changed',
+                text: 'Hello,\n\n' +
+                    'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                req.flash('success', 'Success! Your password has been changed.');
+                done(err);
+            });
+        }
+    ], function(err) {
+        res.redirect('/');
+    });
+});
  
  router.get("/register", function(req,res){
      res.render("register", {error: ""});
@@ -23,7 +145,11 @@ router.get("/", function(req, res){
  router.post("/register", function(req, res){
      let role = {name:"Guest", num:0};
      if(req.body.username === "admin") role = {name: "Admin", num:5};
-     const newUser = new User({username: req.body.username, role: role, registrationDate: Date.now()});
+     const newUser = new User({
+         email: req.body.email,
+         username: req.body.username,
+         role: role, registrationDate: Date.now()
+     });
      User.register(newUser, req.body.password, function(err,user){
          if(err) {
              console.log(err.name);
